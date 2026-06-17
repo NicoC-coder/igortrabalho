4dd245671f186a008c9f1c0c30d3bb0f85f4baf6
NicoC-coder <nicolasantos2303@gmail.com>
2026-06-17 09:01:31 -0300

    Fix route paths by adding backslashes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const auth   = require("../controllers/authController");
 const authMW = require("../middlewares/authMiddleware");
 
-router.post("/register", auth.register);
-router.post("/login",    auth.login);
-router.get("/me",        authMW, auth.me);
+router.post("\register", auth.register);
+router.post("\login",    auth.login);
+router.get("\me",        authMW, auth.me);
 
 module.exports = router;
